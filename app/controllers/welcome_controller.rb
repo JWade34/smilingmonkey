110521e823a3f7b101539276a81651b2d9907391
Justin <justinmckelvey@hotmail.com>
2016-02-19 15:27:17 -0700

    add httparty

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,18 @@
 class WelcomeController < ApplicationController
+  require 'httparty'
+
+  @my_api_key = @lyrics_api
+
+  def index
+    @lyrics_api = ENV["LYRICS_N_MUSIC_API_KEY"]
+
+    url = 'To search for lyrics by all fields: http://api.lyricsnmusic.com/songs?api_key=[my_api_key]'
+    @response = HTTParty.get(url)
+    @response.parsed_response
+  end
+
+  def initialize(lyrics_api)
+    @my_api_key = lyrics_api
+  end
+
 end
