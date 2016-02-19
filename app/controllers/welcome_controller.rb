5a37af8f20702b556d4027163b96c3a7387c559b
Justin <justinmckelvey@hotmail.com>
2016-02-19 16:51:26 -0700

    update api key logic

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,18 +1,14 @@
 class WelcomeController < ApplicationController
   require 'httparty'
 
-  @my_api_key = @lyrics_api
-
-  def index
-    @lyrics_api = ENV["LYRICS_N_MUSIC_API_KEY"]
-
-    url = 'To search for lyrics by all fields: http://api.lyricsnmusic.com/songs?api_key=[my_api_key]'
-    @response = HTTParty.get(url)
-    @response.parsed_response
+  def initialize
+    @my_api_key = ENV["LYRICS_N_MUSIC_API_KEY"]
   end
 
-  def initialize(lyrics_api)
-    @my_api_key = lyrics_api
+  def index
+    url = "http://api.lyricsnmusic.com/songs?api_key=#{ENV["LYRICS_N_MUSIC_API_KEY"]}&artist=coldplay"
+    @response = HTTParty.get(url)
+    @response.parsed_response
   end
 
 end
