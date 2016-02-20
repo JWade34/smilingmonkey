79d453707ec9ceeb9052b2bf4e2dc6678168f37c
Justin <justinmckelvey@hotmail.com>
2016-02-20 16:36:52 -0700

    update welcome controller logic

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,10 +6,12 @@ class WelcomeController < ApplicationController
   end
 
   def index
-    artist = "jayz"
+    # artist = params[:artist]
+    artist =     ["beyonce","jayz","metallica","adele","bieber","taylor%20swift","drake","michael%20jackson","james%20taylor","jimmy%20buffett"].sample
     url = "http://api.lyricsnmusic.com/songs?api_key=#{ENV["LYRICS_N_MUSIC_API_KEY"]}&artist=#{artist}"
     @response = HTTParty.get(url)
-    @response.parsed_response
+    # @response.parsed_response[0..20].sample["snippet"][0..9]
+    @lyrics = @response.parsed_response[0..10].sample
   end
 
 end
