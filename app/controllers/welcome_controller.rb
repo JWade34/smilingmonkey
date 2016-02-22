9c6ee214e0c208ff8b4151969ea8e223b946e03e
Justin <justinmckelvey@hotmail.com>
2016-02-22 08:53:01 -0700

    update params hash

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,7 +11,7 @@ class WelcomeController < ApplicationController
       url = "http://api.lyricsnmusic.com/songs?api_key=#{ENV["LYRICS_N_MUSIC_API_KEY"]}&artist=#{artist}"
       @response = HTTParty.get(url)
       @lyrics = @response.parsed_response[0..10].sample
-      params[:artist] = ""
+      # params[:artist] = ""
     else
       artist = ["beyonce","aretha%20franklin","beatles","adele","bieber","taylor%20swift","frank%20sinatra","michael%20jackson","james%20taylor","jimmy%20buffett"].sample
       url = "http://api.lyricsnmusic.com/songs?api_key=#{ENV["LYRICS_N_MUSIC_API_KEY"]}&artist=#{artist}"
